521d7995b1b580a7d5686422aa96d6871c06b70e
AnwarSahid <anwarsahid28oktober@gmail.com>
2022-11-12 12:02:13 +0700

    routing

--- index.js
+++ index.js
@@ -5,20 +5,47 @@ const requesListener = (request, response) => {
     response.setHeader('Content-type', 'text/html');
     response.statusCode = 200;
 
-    const method = request.method;
+    const { method, url } = request;
 
-    if (method === 'PUT') {
+    // if (method === 'PUT') {
 
-        response.end('halo ini put');
+    //     response.end('halo ini put');
+    // }
+    // if (method === 'POST') {
+    //     let body = [];
+
+    //     request.on('data', (chunk) => {
+    //         body.push(chunk);
+    //     });
+
+    //     request.on('end', () => {
+    //         body = Buffer.concat(body).toString();
+    //         const { name } = JSON.parse(body)
+    //         response.end(`<h1>Hai, ${name}!</h1>`);
+    //     });
+    // }
+
+    // if (method === 'DELETE') {
+    //     response.end('halo ini delete')
+    // }
+
+    // if (method === 'GET') {
+    //     response.end('halo ini get')
+    // }
+
+
+    if (url === '/') {
+        if (method === 'GET') {
+            response.end('<h1>Ini adalah homepage</h1>');
+        }
+    } else if (url === '/about') {
+        response.end("halaman about");
+    } else {
+        response.end("halaman tidak ditemukan");
     }
 
-    if (method === 'DELETE') {
-        response.end('halo ini delete')
-    }
 
-    if (method === 'GET') {
-        response.end('halo ini get')
-    }
+
 
 
 }
